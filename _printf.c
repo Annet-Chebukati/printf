7a3623b4dc6468ee7684a4b113ed09c53aedcc4a
Annet_Chebukati <annetnasimiyuchebukati@gmail.com>
2023-03-25 07:40:24 -0700

    Returns the number of characters printed (excluding the null byte used to end output to strings)

--- _printf.c
+++ _printf.c
@@ -1,45 +1,52 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * _printf - function that produces output according to a format
+ * @format: pointer to format
+ * Return: int
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+	va_list args;
+	int count = 0;
 
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                    putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    count += printf("%s", va_arg(args, char *));
-                    break;
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-    va_end(args);
-    return count;
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					count += printf("%s", va_arg(args, char *));
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
 
