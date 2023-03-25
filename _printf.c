1354b6217d0768c43efe8d8d49377710ed9690c6
Annet_Chebukati <annetnasimiyuchebukati@gmail.com>
2023-03-25 07:19:46 -0700

    produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    count += printf("%s", va_arg(args, char *));
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+    va_end(args);
+    return count;
+}
+
